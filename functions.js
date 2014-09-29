3e40fd74e3e5ed99c8b5f88c3a67dfe9c07a93e7
Tony Gaskell <thgaskell@gmail.com>
2014-09-29 11:38:30 -1000

    Finish adding functions

---
+++ functions.js
@@ -0,0 +1,139 @@
+/**
+ * Converts a number a string.
+ * @param {number} n
+ * @return {string} the number as a string
+ */
+
+/**
+ * Adds one to a given number
+ * @param {number} n
+ * @return {number}
+ */
+
+/**
+ * Subtracts one from a given number
+ * @param {number} n
+ * @return {number}
+ */
+
+/**
+ * Adds two numbers.
+ * @param {number} x
+ * @param {number} y
+ * @return {number} the sum
+ */
+
+/**
+ * Subtracts the second number from the first.
+ * @param {number} x
+ * @param {number} y
+ * @return {number} the difference
+ */
+
+/**
+ * Multiplies two numbers.
+ * @param {number} x
+ * @param {number} y
+ * @return {number} the product
+ */
+
+/**
+ * Divides the first number by the second.
+ * @param {number} x
+ * @param {number} y
+ * @return {number} the quotient
+ */
+
+/**
+ * Multiplies a number by itself.
+ * @param {number} x, number to be squared
+ * @return {number} squared
+ */
+
+/**
+ * Prints out the equation: (i.e.) "1 + 5 = 6" or "8 / 2 = 4".
+ * Returns the result.
+ * @param {string} operation "add", "subtract", "multiply", or "divide"
+ * @param {number} x
+ * @param {number} y
+ * @return {number} the result
+ */
+
+/**
+ * Returns true if x is greater than y
+ * @param {number} x
+ * @param {number} y
+ * @return {boolean} x is larger than y
+ */
+
+/**
+ * Returns true if x is less than y
+ * @param {number} x
+ * @param {number} y
+ * @return {boolean} x is smaller than y
+ */
+
+/**
+ * Returns true if a and b are equal
+ * @param {number} x
+ * @param {number} y
+ * @return {boolean} the numbers are equal
+ */
+
+/**
+ * Returns the smallest value of two numbers.
+ * @param {number} a
+ * @param {number} b
+ * @return {number} the smallest number
+ */
+
+/**
+ * Returns the largest value of two numbers.
+ * @param {number} a
+ * @param {number} b
+ * @return {number} the largest number
+ */
+
+/**
+ * Returns true if `n` is even.
+ * @param {number} n
+ * @return {boolean} the number is even
+ */
+
+/**
+ * Returns true if `n` is odd.
+ * @param {number} n
+ * @return {boolean} the number is odd
+ */
+
+/**
+ * Returns a letter grade.
+ * "A": 90-100%
+ * "B": 80-89%
+ * "C": 70-79%
+ * "D": 60-69%
+ * "F": 0-59%
+ * @param {number} score
+ * @param {number} total maximum possible score
+ * @return {string} the score represented as a letter grade
+ */
+
+/**
+ * Checks if arestaurant` object has are `views` field.
+ * If it does, increase it by 1. If it does not,
+ * set itsreviews` field to 1.
+ * @param {object} restaurant represents a restaurant
+ */
+
+/**
+ * Joins two strings with a space.
+ * @param {string} word1
+ * @param {string} word2
+ * @return {string} joined the words joined with a space
+ */
+
+/**
+ * Returns a circle object with the properties `circumferance` and `area`.
+ * @param {number} radius
+ * @return {object} circle
+ */
