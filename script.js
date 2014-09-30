ef8288de5d698c3007a8a47d53b8425e31cdad54
Tony Gaskell <thgaskell@gmail.com>
2014-09-29 22:52:06 -1000

    Make sure script.js runs after tests have been ran

--- script.js
+++ script.js
@@ -1,45 +1,49 @@
-/**
- * Follow the comments below.
- * Feel free to use `console.log` to see what values are being returned.
+// Please make sure all your code is added inside the `runAfterTests` function.
+function runAfterTests() {
+  /**
+   * Follow the comments below.
+   * Feel free to use `console.log` to see what values are being returned.
+   */
 
-// Create a variable called `myNumber`, and assign it to any number
+  // Create a variable called `myNumber`, and assign it to any number
 
-/**
- * Create another variable called `stringValue`.
- * Assign it the value that's returned when you pass
- * `myNumber into the function `numberToString`.
- */
+  /**
+   * Create another variable called `stringValue`.
+   * Assign it the value that's returned when you pass
+   * `myNumber into the function `numberToString`.
+   */
 
-/**
- * If you pass `myNumber` and `stringValue` into `areEqual`,
- * what value will you get back? Why is that?
- */
+  /**
+   * If you pass `myNumber` and `stringValue` into `areEqual`,
+   * what value will you get back? Why is that?
+   */
 
-/**
- * You may have noticed that the `calculate` function combined
- * the functionality of `add`, `subtract`, `multiply`, and `divide`.
- * Did you try to use those functions _inside_ of `calculate`?
- * If you didn't go back and try it, this is called "reusability"!
- */
+  /**
+   * You may have noticed that the `calculate` function combined
+   * the functionality of `add`, `subtract`, `multiply`, and `divide`.
+   * Did you try to use those functions _inside_ of `calculate`?
+   * If you didn't go back and try it, this is called "reusability"!
+   */
 
-// Do you see any other functions where code could be reused?
+  // Do you see any other functions where code could be reused?
 
-/**
- * For your `letterGrade` function,
- * what happens if you provide a `score` that is less than 0,
- * or greater than the `total` value?
- * Does it still work as expected? If it doesn't, how could you improve this?
- */
+  /**
+   * For your `letterGrade` function,
+   * what happens if you provide a `score` that is less than 0,
+   * or greater than the `total` value?
+   * Does it still work as expected? If it doesn't, how could you improve this?
+   */
 
-/**
- * Your `combine` function looks okay,
- * but what if you wanted to join it with ANY string instead of a single space?
- * Create a new function called `improvedCombine`,
- * with the same parameters as `combine`, but also add a
- * third parameter called `glue` (a string value).
- */
+  /**
+   * Your `combine` function looks okay,
+   * but what if you wanted to join it with ANY string instead of a single space?
+   * Create a new function called `improvedCombine`,
+   * with the same parameters as `combine`, but also add a
+   * third parameter called `glue` (a string value).
+   */
 
-// What happens if you were to uncomment the following lines?
+  // What happens if you were to uncomment the following lines?
 
-// combine = improvedCombine;
-// console.log(combine('combine', 'improved!', ' is '));
+  // combine = improvedCombine;
+  // console.log(combine('combine', 'improved!', ' is '));
+}
