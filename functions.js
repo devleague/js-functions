9ba1c7cb2e4383ffba3ce42d155c867808fe9ceb
Ray Farias <ray@rayfarias.com>
2015-04-29 16:49:37 -1000

    convert repository to run in cli instead of a web browser

--- functions.js
+++ functions.js
@@ -3,9 +3,7 @@
  * @param {number} n
  * @return {string} the number as a string
  */
-function numberToString(num) {
-  return num + '';
-}
+
 
 /**
  * Adds one to a given number.
@@ -13,62 +11,53 @@ function numberToString(num) {
  * @return {number}
  */
 
-function increase(num) {
-  return num + 1;
-}
 
 /**
  * Subtracts one from a given number.
  * @param {number} n
  * @return {number}
  */
-function decrease(num) {
-  return num - 1;
-}
+
+
 /**
  * Adds two numbers.
  * @param {number} x
  * @param {number} y
  * @return {number} the sum
  */
-function add(a, b) {
-  return a + b;
-}
+
+
 /**
  * Subtracts the second number from the first.
  * @param {number} x
  * @param {number} y
  * @return {number} the difference
  */
-function subtract(a, b){
-  return a - b;
-}
+
+
 /**
  * Multiplies two numbers.
  * @param {number} x
  * @param {number} y
  * @return {number} the product
  */
-function multiply(a, b){
-  return a * b;
-}
+
+
 /**
  * Divides the first number by the second.
  * @param {number} x
  * @param {number} y
  * @return {number} the quotient
  */
-function divide(a, b){
-  return a / b;
-}
+
+
 /**
  * Multiplies a number by itself.
  * @param {number} x, number to be squared
  * @return {number} squared
  */
-function square(num){
-  return num * num;
-}
+
+
 /**
  * Performs a mathematical operation on two numbers.
  * Also prints out the equation: (i.e.) "1 + 5 = 6" or "8 / 2 = 4".
@@ -77,115 +66,62 @@ function square(num){
  * @param {number} y
  * @return {number} the result
  */
-function calculate(op, a, b){
-  var result = 0;
-  switch(op) {
-    case 'add':
-      result = add(a, b);
-      console.log(a + ' + ' + b + ' = ' + result);
-      return result;
-      break;
-    case 'subtract':
-      result = subtract(a, b);
-      console.log(a + ' - ' + b + ' = ' + result);
-      return result;
-      break;
-    case 'multiply':
-      result = multiply(a, b);
-      console.log(a + ' * ' + b + ' = ' + result);
-      return result;
-      break;
-    case 'divide':
-      result = divide(a, b);
-      console.log(a + ' / ' + b + ' = ' + result);
-      return result;
-      break;
-    default:
-      alert("wraong");
-  }
-}
+
+
 /**
  * Returns true if `a` is greater than `b`.
  * @param {number} a
  * @param {number} b
  * @return {boolean} `a` is larger than `b`
  */
-function isGreaterThan(a, b) {
-  if (a > b) {
-    return true;
-  }
-  return false;
-}
+
+
 /**
  * Returns true if `a` is less than `b`.
  * @param {number} a
  * @param {number} b
  * @return {boolean} `a` is smaller than `b`
  */
-function isLessThan(a, b){
-  if (a < b) {
-    return true;
-  }
-  return false;
-}
+
+
 /**
  * Returns true if `a` and `b` are equal.
  * @param {number} a
  * @param {number} b
  * @return {boolean} the numbers are equal
  */
-function areEqual(a, b) {
-  if (a === b) {
-    return true;
-  }
-  return false;
-}
+
+
 /**
  * Returns the smallest value of two numbers.
  * @param {number} x
  * @param {number} y
  * @return {number} the smallest number
  */
-function minimum(a, b) {
-  if (a > b) {
-    return b;
-  }
-  return a;
-}
+
+
 /**
  * Returns the largest value of two numbers.
  * @param {number} x
  * @param {number} y
  * @return {number} the largest number
  */
-function maximum(a, b) {
-  if (a > b) {
-    return a;
-  }
-  return b;
-}
+
+
 /**
  * Returns true if `n` is even.
  * @param {number} n
  * @return {boolean} the number is even
  */
-function isEven(number) {
-  if (number % 2 === 0) {
-    return true;
-  }
-  return false;
-}
+
+
 /**
  * Returns true if `n` is odd.
  * @param {number} n
  * @return {boolean} the number is odd
  */
-function isOdd(number) {
-  if (number % 2 !== 0) {
-    return true;
-  }
-  return false;
-}
+
+
 /**
  * Returns a letter grade.
  * "A": 90-100%
@@ -197,26 +133,7 @@ function isOdd(number) {
  * @param {number} total maximum possible score
  * @return {string} the score represented as a letter grade
  */
-function letterGrade(score, max_score) {
-  var result = score / max_score;
-  if (result > 0.89) {
-    return "A";
-  }
 
-  if (result <= 0.89 && result > 0.79) {
-    return "B";
-  }
-
-  if (result <= 0.79 && result > 0.69999) {
-    return "C";
-  }
-
-  if (result <= 0.69998 && result > 0.59) {
-    return "D";
-  }
-
-  return "F";
-}
 
 /**
  * Checks if a `restaurant` object has a `reviews` field.
@@ -224,23 +141,16 @@ function letterGrade(score, max_score) {
  * set itsreviews` field to 1.
  * @param {object} restaurant represents a restaurant
  */
-function incrementReviews(restaurant) {
-  if (restaurant.reviews) {
-    restaurant.reviews++;
-  } else {
-    restaurant.reviews = 1;
-  }
-  return restaurant;
-}
+
+
 /**
  * Joins two strings with a space.
  * @param {string} word1
  * @param {string} word2
  * @return {string} joined the words joined with a space
  */
-function combine(str1, str2) {
-  return str1 + ' ' + str2;
-}
+
+
 /**
  * Returns a circle object with the properties `circumference` and `area`.
  * Use Math.PI for the value π.
@@ -248,9 +158,4 @@ function combine(str1, str2) {
  * @param {number} radius
  * @return {object} circle
  */
-function createCircle(radius) {
-  var circle = {};
-  circle.circumference = 2 * Math.PI * radius;
-  circle.area = Math.PI * radius * radius;
-  return circle;
-}
+
--- test/test-script.js
+++ test/test-script.js
@@ -1,5 +1,13 @@
+var fs = require('fs');
+var vm = require('vm');
+var sinon = require('sinon');
+var chai = require('chai');
 var expect = chai.expect;
 var should = chai.should();
+
+var functionsFile = fs.readFileSync(process.cwd() + '/functions.js');
+vm.runInThisContext(functionsFile); // file runs and it's contents has access to GLOBAL
+
 describe("Main", function() {
   var sandbox;
 
@@ -8,8 +16,8 @@ describe("Main", function() {
     sandbox = sinon.sandbox.create();
 
     // stub some console methods
-    sandbox.stub(window.console, "log");
-    sandbox.stub(window.console, "error");
+    sandbox.stub(console, "log");
+    sandbox.stub(console, "error");
   });
 
   afterEach(function() {
@@ -19,19 +27,19 @@ describe("Main", function() {
 
   describe("#numberToString", function() {
     it("should be a function", function() {
-      (typeof window.numberToString).should.equal("function");
+      (typeof GLOBAL.numberToString).should.equal("function");
     });
 
     it("should convert the given number to a string", function() {
-      var stringNum = numberToString(5);
+      var stringNum = GLOBAL.numberToString(5);
       expect(stringNum).to.be.a("string");
       expect(stringNum).to.equal("5");
 
-      stringNum = numberToString(-260/500);
+      stringNum = GLOBAL.numberToString(-260/500);
       expect(stringNum).to.be.a("string");
       expect(stringNum).to.equal("-0.52");
 
-      stringNum = numberToString(3.14159265359);
+      stringNum = GLOBAL.numberToString(3.14159265359);
       expect(stringNum).to.be.a("string");
       expect(stringNum).to.equal("3.14159265359");
     });
@@ -39,228 +47,228 @@ describe("Main", function() {
 
   describe("#increase", function() {
     it("should be a function", function() {
-      (typeof window.increase).should.equal("function");
+      (typeof GLOBAL.increase).should.equal("function");
     });
 
     it("should add 1 to the given number", function() {
-      expect(increase(7)).to.equal(8);
-      expect(increase(-1)).to.equal(0);
-      expect(increase(-100)).to.equal(-99);
+      expect(GLOBAL.increase(7)).to.equal(8);
+      expect(GLOBAL.increase(-1)).to.equal(0);
+      expect(GLOBAL.increase(-100)).to.equal(-99);
     });
   });
 
   describe("#decrease", function() {
     it("should be a function", function() {
-      (typeof window.decrease).should.equal("function");
+      (typeof GLOBAL.decrease).should.equal("function");
     });
 
     it("should subtract 1 from the given number", function() {
-      expect(decrease(7)).to.equal(6);
-      expect(decrease(-1)).to.equal(-2);
-      expect(decrease(-100)).to.equal(-101);
+      expect(GLOBAL.decrease(7)).to.equal(6);
+      expect(GLOBAL.decrease(-1)).to.equal(-2);
+      expect(GLOBAL.decrease(-100)).to.equal(-101);
     });
   });
 
   describe("#add", function() {
     it("should be a function", function() {
-      (typeof window.add).should.equal("function");
+      (typeof GLOBAL.add).should.equal("function");
     });
 
     it("should add two numbers", function() {
-      expect(add(9, 3)).to.equal(12);
-      expect(add(0, -272.15)).to.equal(-272.15);
-      expect(add(-1, -100)).to.equal(-101);
+      expect(GLOBAL.add(9, 3)).to.equal(12);
+      expect(GLOBAL.add(0, -272.15)).to.equal(-272.15);
+      expect(GLOBAL.add(-1, -100)).to.equal(-101);
     });
   });
 
   describe("#subtract", function() {
     it("should be a function", function() {
-      (typeof window.subtract).should.equal("function");
+      (typeof GLOBAL.subtract).should.equal("function");
     });
 
     it("should subtract the second number from the first", function() {
-      expect(subtract(9, 3)).to.equal(6);
-      expect(subtract(3, 9)).to.equal(-6);
-      expect(subtract(-1, -100)).to.equal(99);
+      expect(GLOBAL.subtract(9, 3)).to.equal(6);
+      expect(GLOBAL.subtract(3, 9)).to.equal(-6);
+      expect(GLOBAL.subtract(-1, -100)).to.equal(99);
     });
   });
 
   describe("#multiply", function() {
     it("should be a function", function() {
-      (typeof window.multiply).should.equal("function");
+      (typeof GLOBAL.multiply).should.equal("function");
     });
 
     it("should multiply two numbers", function() {
-      expect(multiply(9, 3)).to.equal(27);
-      expect(multiply(0, -8)).to.equal(0);
-      expect(multiply(-4, 3)).to.equal(-12);
+      expect(GLOBAL.multiply(9, 3)).to.equal(27);
+      expect(GLOBAL.multiply(0, -8)).to.equal(0);
+      expect(GLOBAL.multiply(-4, 3)).to.equal(-12);
     });
   });
 
   describe("#divide", function() {
     it("should be a function", function() {
-      (typeof window.divide).should.equal("function");
+      (typeof GLOBAL.divide).should.equal("function");
     });
 
     it("should divide the first number by the second", function() {
-      expect(divide(0, 3)).to.equal(0);
-      expect(divide(3, -12)).to.equal(-0.25);
-      expect(divide(7, 0)).to.equal(Infinity);
+      expect(GLOBAL.divide(0, 3)).to.equal(0);
+      expect(GLOBAL.divide(3, -12)).to.equal(-0.25);
+      expect(GLOBAL.divide(7, 0)).to.equal(Infinity);
     });
   });
 
   describe("#square", function() {
     it("should be a function", function() {
-      (typeof window.square).should.equal("function");
+      (typeof GLOBAL.square).should.equal("function");
     });
 
     it("should return the square of the given number", function() {
-      expect(square(12)).to.equal(144);
-      expect(square(-3)).to.equal(9);
-      expect(square(1/4)).to.equal(0.0625);
+      expect(GLOBAL.square(12)).to.equal(144);
+      expect(GLOBAL.square(-3)).to.equal(9);
+      expect(GLOBAL.square(1/4)).to.equal(0.0625);
     });
   });
 
   describe("#calculate", function() {
     it("should be a function", function() {
-      (typeof window.calculate).should.equal("function");
+      (typeof GLOBAL.calculate).should.equal("function");
     });
 
     it("should print the equation to the console", function() {
-      calculate("add", 4, 7);
+      GLOBAL.calculate("add", 4, 7);
       sinon.assert.calledWithExactly(console.log, "4 + 7 = 11");
 
-      calculate("subtract", 4, 7);
+      GLOBAL.calculate("subtract", 4, 7);
       sinon.assert.calledWithExactly(console.log, "4 - 7 = -3");
 
-      calculate("multiply", 8, 4);
+      GLOBAL.calculate("multiply", 8, 4);
       sinon.assert.calledWithExactly(console.log, "8 * 4 = 32");
 
-      calculate("divide", 8, 4);
+      GLOBAL.calculate("divide", 8, 4);
       sinon.assert.calledWithExactly(console.log, "8 / 4 = 2");
     });
 
     it("should return the result of the equation", function() {
-      expect(calculate("add", 4, 7)).to.equal(11);
-      expect(calculate("subtract", 4, 7)).to.equal(-3);
-      expect(calculate("multiply", 8, 4)).to.equal(32);
-      expect(calculate("divide", 8, 4)).to.equal(2);
+      expect(GLOBAL.calculate("add", 4, 7)).to.equal(11);
+      expect(GLOBAL.calculate("subtract", 4, 7)).to.equal(-3);
+      expect(GLOBAL.calculate("multiply", 8, 4)).to.equal(32);
+      expect(GLOBAL.calculate("divide", 8, 4)).to.equal(2);
     });
   });
 
   describe("#isGreaterThan", function() {
     it("should be a function", function() {
-      (typeof window.isGreaterThan).should.equal("function");
+      (typeof GLOBAL.isGreaterThan).should.equal("function");
     });
 
     it("should return true if x is greater than y", function() {
-      expect(isGreaterThan(7, 4)).to.equal(true);
-      expect(isGreaterThan(-45, -31)).to.equal(false);
-      expect(isGreaterThan(-9, -9)).to.equal(false);
+      expect(GLOBAL.isGreaterThan(7, 4)).to.equal(true);
+      expect(GLOBAL.isGreaterThan(-45, -31)).to.equal(false);
+      expect(GLOBAL.isGreaterThan(-9, -9)).to.equal(false);
     });
   });
 
   describe("#isLessThan", function() {
     it("should be a function", function() {
-      (typeof window.isLessThan).should.equal("function");
+      (typeof GLOBAL.isLessThan).should.equal("function");
     });
 
     it("should return true if x is less than y", function() {
-      expect(isLessThan(4, 7)).to.equal(true);
-      expect(isLessThan(19, -31)).to.equal(false);
-      expect(isLessThan(28, 28)).to.equal(false);
+      expect(GLOBAL.isLessThan(4, 7)).to.equal(true);
+      expect(GLOBAL.isLessThan(19, -31)).to.equal(false);
+      expect(GLOBAL.isLessThan(28, 28)).to.equal(false);
     });
   });
 
   describe("#areEqual", function() {
     it("should be a function", function() {
-      (typeof window.areEqual).should.equal("function");
+      (typeof GLOBAL.areEqual).should.equal("function");
     });
 
     it("should return true if a and b have equal values and data types", function() {
-      expect(areEqual(1, 1)).to.equal(true);
-      expect(areEqual(-1/4, -0.25)).to.equal(true);
+      expect(GLOBAL.areEqual(1, 1)).to.equal(true);
+      expect(GLOBAL.areEqual(-1/4, -0.25)).to.equal(true);
     });
 
     it("should return false if values are equal but data types are different", function() {
-      expect(areEqual(21, "21")).to.equal(false);
+      expect(GLOBAL.areEqual(21, "21")).to.equal(false);
     });
   });
 
   describe("#minimum", function() {
     it("should be a function", function() {
-      (typeof window.minimum).should.equal("function");
+      (typeof GLOBAL.minimum).should.equal("function");
     });
 
     it("should return the smallest of two values", function() {
-      expect(minimum(71, 35)).to.equal(35);
-      expect(minimum(-40, -8)).to.equal(-40);
-      expect(minimum(-1, -1)).to.equal(-1);
+      expect(GLOBAL.minimum(71, 35)).to.equal(35);
+      expect(GLOBAL.minimum(-40, -8)).to.equal(-40);
+      expect(GLOBAL.minimum(-1, -1)).to.equal(-1);
     });
   });
 
   describe("#maximum", function() {
     it("should be a function", function() {
-      (typeof window.maximum).should.equal("function");
+      (typeof GLOBAL.maximum).should.equal("function");
     });
 
     it("should return the largest of two values", function() {
-      expect(maximum(35, 71)).to.equal(71);
-      expect(maximum(-40, -8)).to.equal(-8);
-      expect(maximum(-1, -1)).to.equal(-1);
+      expect(GLOBAL.maximum(35, 71)).to.equal(71);
+      expect(GLOBAL.maximum(-40, -8)).to.equal(-8);
+      expect(GLOBAL.maximum(-1, -1)).to.equal(-1);
     });
   });
 
   describe("#isEven", function() {
     it("should be a function", function() {
-      (typeof window.isEven).should.equal("function");
+      (typeof GLOBAL.isEven).should.equal("function");
     });
 
     it("should return true if the given number is even", function() {
-      expect(2).to.satisfy(isEven);
-      expect(-8).to.satisfy(isEven);
-      expect(256).to.satisfy(isEven);
+      expect(2).to.satisfy(GLOBAL.isEven);
+      expect(-8).to.satisfy(GLOBAL.isEven);
+      expect(256).to.satisfy(GLOBAL.isEven);
     });
   });
 
   describe("#isOdd", function() {
     it("should be a function", function() {
-      (typeof window.isOdd).should.equal("function");
+      (typeof GLOBAL.isOdd).should.equal("function");
     });
 
     it("should return true if the given number is odd", function() {
-      expect(1).to.satisfy(isOdd);
-      expect(-23).to.satisfy(isOdd);
-      expect(97).to.satisfy(isOdd);
+      expect(1).to.satisfy(GLOBAL.isOdd);
+      expect(-23).to.satisfy(GLOBAL.isOdd);
+      expect(97).to.satisfy(GLOBAL.isOdd);
     });
   });
 
   describe("#letterGrade", function() {
     it("should be a function", function() {
-      (typeof window.letterGrade).should.equal("function");
+      (typeof GLOBAL.letterGrade).should.equal("function");
     });
 
     it("should return a letter grade based on score/total percentage", function() {
-      expect(letterGrade(315, 350)).to.equal("A");
-      expect(letterGrade(287, 350)).to.equal("B");
-      expect(letterGrade(105, 150)).to.equal("C");
-      expect(letterGrade(559, 800)).to.equal("D");
-      expect(letterGrade(2, 4)).to.equal("F");
+      expect(GLOBAL.letterGrade(315, 350)).to.equal("A");
+      expect(GLOBAL.letterGrade(287, 350)).to.equal("B");
+      expect(GLOBAL.letterGrade(105, 150)).to.equal("C");
+      expect(GLOBAL.letterGrade(559, 800)).to.equal("D");
+      expect(GLOBAL.letterGrade(2, 4)).to.equal("F");
     });
   });
 
   describe("#incrementReviews", function() {
     it("should be a function", function() {
-      (typeof window.incrementReviews).should.equal("function");
+      (typeof GLOBAL.incrementReviews).should.equal("function");
     });
 
     it("should increment the reviews by 1, if property exists", function() {
-      var awesomeEatery = incrementReviews({reviews: 1776});
+      var awesomeEatery = GLOBAL.incrementReviews({reviews: 1776});
       expect(awesomeEatery.reviews).to.equal(1777);
     });
 
     it("should set the reviews to 1, if property does not exist", function() {
-      var newEatery = incrementReviews({});
+      var newEatery = GLOBAL.incrementReviews({});
       expect(newEatery).to.have.property("reviews");
       expect(newEatery.reviews).to.equal(1);
     });
@@ -268,15 +276,15 @@ describe("Main", function() {
 
   describe("#combine", function() {
     it("should be a function", function() {
-      (typeof window.combine).should.equal("function");
+      (typeof GLOBAL.combine).should.equal("function");
     });
 
     it("should return a string of the words joined with a space", function() {
-      var result = combine("saving", "galaxies");
+      var result = GLOBAL.combine("saving", "galaxies");
       expect(result).to.be.a("string");
       expect(result).to.equal("saving galaxies");
 
-      result = combine("with", "groot");
+      result = GLOBAL.combine("with", "groot");
       expect(result).to.be.a("string");
       expect(result).to.equal("with groot");
     });
@@ -287,9 +295,9 @@ describe("Main", function() {
 
     it("should be a function", function(done) {
       setTimeout(function() {
-        (typeof window.createCircle).should.equal("function");
+        (typeof GLOBAL.createCircle).should.equal("function");
         radius = 4;
-        circle = createCircle(radius);
+        circle = GLOBAL.createCircle(radius);
         done();
       }, 1000);
     });
