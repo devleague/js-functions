2bfe75ed0510cba21bd38635c647b4335dfdbc4b
Gail Matsushima <gail.matsushima@gmail.com>
2015-11-03 23:37:45 -1000

    up to letterGrade passes

--- functions.js
+++ functions.js
@@ -5,10 +5,8 @@
  */
 function numberToString (number) {
 
-if (typeof number) {return number.toString() + number++;}
-
-}
-console.log(numberToString(456));
+  return number.toString();
+ }
 /**
  * Adds one to a given number.
  * @param {number} n
@@ -17,10 +15,9 @@ console.log(numberToString(456));
 
 function increase (number) {
 
-if (typeof number) {return number +1;}
+  return number +1;
+ }
 
-}
-console.log(increase(456));
 
 /**
  * Subtracts one from a given number.
@@ -28,12 +25,11 @@ console.log(increase(456));
  * @return {number}
  */
 
-function decrease (number) {
+function decrease (n) {
 
-if (typeof number) {return number -1;}
-
+  return n -1;
 }
-console.log(decrease(456));
+
 
 /**
  * Adds two numbers.
@@ -41,12 +37,11 @@ console.log(decrease(456));
  * @param {number} y
  * @return {number} the sum
  */
-function add ( x, y) {
+function add (x, y) {
+  return x + y;
+ }
 
-if (typeof number) {return x + y;}
-
-}
-console.log(add( 10, 20));
+add( 10, 20);
 
 /**
  * Subtracts the second number from the first.
@@ -54,12 +49,10 @@ console.log(add( 10, 20));
  * @param {number} y
  * @return {number} the difference
  */
-function substract ( x, y) {
+function subtract (x, y) {
 
-if (typeof number) {return y - x;}
-
-}
-console.log(substract( 10, 20));
+  return x - y;
+ }
 
 /**
  * Multiplies two numbers.
@@ -67,12 +60,11 @@ console.log(substract( 10, 20));
  * @param {number} y
  * @return {number} the product
  */
-function multiply ( x, y) {
+function multiply (x, y) {
 
-if (typeof number) {return x*y;}
-
+  return x * y;
 }
-console.log(multiply( 10, 20));
+
 
 /**
  * Divides the first number by the second.
@@ -80,12 +72,9 @@ console.log(multiply( 10, 20));
  * @param {number} y
  * @return {number} the quotient
  */
-function divide ( x, y) {
-
-if (typeof number) {return x / y;}
-
-}
-console.log(divide( 100, 20));
+function divide (x, y) {
+  return x / y;
+ }
 
 /**
  * Multiplies a number by itself.
@@ -93,27 +82,40 @@ console.log(divide( 100, 20));
  * @return {number} squared
  */
 function square (x) {
+  return x * x;
+ }
 
-if (typeof number) {return x * x;}
-
-}
-console.log(square(2));
-
-/**
+/*
  * Performs a mathematical operation on two numbers.
  * Also prints out the equation: (i.e.) "1 + 5 = 6" or "8 / 2 = 4".
  * @param {string} operation "add", "subtract", "multiply", or "divide"
  * @param {number} x
  * @param {number} y
- * @return {number} the result
- */
-function calculate ( x, y) {
+ * @return {number} the result*/
 
-if (typeof number) {return (x + y) + (x * y);}
+function calculate (operation, x, y) {
 
+   if (operation === "add") {
+    var sum = x + y;
+    console.log (x + " + " + y + " = " + sum);
+   return sum;
+   }
+   else if (operation === "subtract") {
+    var minus = x - y;
+    console.log (x + " - " + y + " = " + minus);
+   return minus;
+   }
+   else if (operation === "multiply") {
+   var times = x * y;
+   console.log(x + " * " + y + " = " + times);
+   return times;
+   }
+   else if (operation === "divide") {
+   var divvy = x / y;
+   console.log(x + " / " + y + " = " + divvy);
+   return divvy;
+  }
 }
-console.log(calculate(5, 6));
-
 
 /**
  * Returns true if `a` is greater than `b`.
@@ -122,14 +124,26 @@ console.log(calculate(5, 6));
  * @return {boolean} `a` is larger than `b`
  */
 
-
+function isGreaterThan (a, b) {
+  if (a > b) {
+    return true;
+  }
+  else {return false;
+  }
+}
 /**
  * Returns true if `a` is less than `b`.
  * @param {number} a
  * @param {number} b
  * @return {boolean} `a` is smaller than `b`
  */
-
+function isLessThan (a, b) {
+  if (a < b) {
+    return true;
+  }
+  else {return false;
+  }
+}
 
 /**
  * Returns true if `a` and `b` are equal.
@@ -137,7 +151,14 @@ console.log(calculate(5, 6));
  * @param {number} b
  * @return {boolean} the numbers are equal
  */
-
+function areEqual (a, b) {
+  if (a === b) {
+    return true;
+  }
+  else {
+  return false;
+  }
+}
 
 /**
  * Returns the smallest value of two numbers.
@@ -145,7 +166,16 @@ console.log(calculate(5, 6));
  * @param {number} y
  * @return {number} the smallest number
  */
+function minimum (a, b) {
 
+  var arr = [a, b];
+
+  if (typeof number) {
+   if (arr.sort()) {
+     return arr[0];
+   }
+  }
+}
 
 /**
  * Returns the largest value of two numbers.
@@ -153,21 +183,47 @@ console.log(calculate(5, 6));
  * @param {number} y
  * @return {number} the largest number
  */
+function maximum (a, b) {
 
+  //var arr = [1, 2, 24, 34, 100, -45];
+  var arr = [a, b];
+
+  //this e.g. arr.sort(function(a, b) {if (a > b) {return 1;} else if (b > a) {return -1;} else {return 0;}})
+  //this function is returning in ascending, which is why arr[1]
+  //able to switch operators, +1 or -1, or parameters to make array go in ascending or descending order
+  arr.sort(function (a, b){
+   return (a - b);
+  });
+   return arr[1];
+}
 
 /**
  * Returns true if `n` is even.
  * @param {number} n
  * @return {boolean} the number is even
  */
+function isEven (number) {
 
+ if (number % 2 === 0) {
+  return true;
+ }
+ else {return false;
+ }
+}
 
 /**
  * Returns true if `n` is odd.
  * @param {number} n
  * @return {boolean} the number is odd
  */
+function isOdd (number) {
 
+ if (number % 2 !== 0) {
+  return true;
+ }
+ else {return false;
+ }
+}
 
 /**
  * Returns a letter grade.
@@ -180,7 +236,28 @@ console.log(calculate(5, 6));
  * @param {number} total maximum possible score
  * @return {string} the score represented as a letter grade
  */
+function letterGrade (score, total) {
 
+  var percentage = (score / total) * 100;
+//<100 vs <= 100 as this will make you miss 99.89 etc etc
+  if (percentage >= 90 && percentage < 100) {
+    return "A";
+  }
+  else if (percentage >= 80 && percentage < 90) {
+    return "B";
+  }
+  else if (percentage >= 70 && percentage < 80) {
+    return "C";
+  }
+  else if (percentage >= 60 && percentage < 70) {
+    return "D";
+  }
+  else if (percentage >= 0 && percentage < 60) {
+    return "F";
+  }
+}
+
+  // body...
 
 /**
  * Checks if a `restaurant` object has a `reviews` property.
@@ -189,6 +266,19 @@ console.log(calculate(5, 6));
  * @param {object} restaurant   represents a restaurant object
  * @return {object} restaurant
  */
+/*function incrementReviews (restaurant) {
+
+  var eatery = {
+
+    reviews: 0,
+    name: name,
+    location:
+  }
+  for (var i = 0; i < eatery; i++) {
+
+  }
+
+};*/
 
 
 /**
