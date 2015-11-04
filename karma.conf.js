55aab906bcbd1674af86ddfaa9e18a20acdf437e
sgnl <rayrfarias@gmail.com>
2015-11-04 09:01:49 -1000

    Add Travis CI

---
+++ karma.conf.js
@@ -0,0 +1,17 @@
+module.exports = function (config) {
+  config.set({
+      basePath: '',
+      frameworks: ['mocha', 'chai', 'sinon', 'sinon-chai'],
+      files: [
+        'test/*.spec.js',
+        'functions.js'
+      ],
+      reporters: ['progress'],
+      port: 9876,
+      colors: true,
+      autoWatch: true,
+      singleRun: false,
+      logLevel: config.LOG_INFO,
+      browsers: ['PhantomJS']
+  });
+};
