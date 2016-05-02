2f217300b67d14586aa254db73a159abb4eca42b
Bryce <bsaito88@gmail.com>
2016-05-02 01:34:25 -1000

    Added #numberToString, #increase, #decrease, #add, #subtract, #multiply, #divide, #square, and #calculate functions.

--- functions.js
+++ functions.js
@@ -3,21 +3,26 @@
  * @param {number} n
  * @return {string} the number as a string
  */
-
+function numberToString(Number) {
+  return Number.toString();
+}
 
 /**
  * Adds one to a given number.
  * @param {number} n
  * @return {number}
  */
-
-
+function increase(n) {
+  return n += 1;
+}
 /**
  * Subtracts one from a given number.
  * @param {number} n
  * @return {number}
  */
-
+function decrease(n) {
+  return n -= 1;
+}
 
 /**
  * Adds two numbers.
@@ -25,7 +30,9 @@
  * @param {number} y
  * @return {number} the sum
  */
-
+function add(x,y) {
+  return x + y;
+}
 
 /**
  * Subtracts the second number from the first.
@@ -33,7 +40,9 @@
  * @param {number} y
  * @return {number} the difference
  */
-
+function subtract(x,y) {
+  return x - y;
+}
 
 /**
  * Multiplies two numbers.
@@ -41,7 +50,9 @@
  * @param {number} y
  * @return {number} the product
  */
-
+function multiply(x,y) {
+  return x * y;
+}
 
 /**
  * Divides the first number by the second.
@@ -49,14 +60,18 @@
  * @param {number} y
  * @return {number} the quotient
  */
-
+function divide(x,y) {
+  return x / y;
+}
 
 /**
  * Multiplies a number by itself.
  * @param {number} x, number to be squared
  * @return {number} squared
  */
-
+function square(x) {
+  return x * x;
+}
 
 /**
  * Performs a mathematical operation on two numbers.
@@ -66,7 +81,25 @@
  * @param {number} y
  * @return {number} the result
  */
-
+function calculate(operation, x,y) {
+  if (operation === 'add') {
+      var sum = x + y;
+      console.log(x + ' + ' + y + ' = ' + sum);
+      return sum;
+  } else if (operation === 'subtract'){
+      var diff = x - y;
+      console.log(x + ' - ' + y + ' = ' + diff);
+      return diff;
+  } else if (operation === 'multiply') {
+      var prod = x * y;
+      console.log(x + ' * ' + y + ' = ' + prod);
+      return prod;
+  } else if (operation === 'divide') {
+      var quo = x / y;
+      console.log(x + ' / ' + y + ' = ' + quo);
+      return quo;
+  }
+}
 
 /**
  * Returns true if `a` is greater than `b`.
