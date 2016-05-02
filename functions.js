617cefbef48274adfbdeacd2401b55f2ede3e806
Lisa <misslisazhou@gmail.com>
2016-05-01 22:11:34 -1000

    create letterGrade function

--- functions.js
+++ functions.js
@@ -192,7 +192,20 @@ function isOdd(n) {
  * @param {number} total maximum possible score
  * @return {string} the score represented as a letter grade
  */
-
+function letterGrade(score, total) {
+  var percentage = divide(score, total) * 100;
+  if (percentage >=90){
+    return "A";
+  }else if(percentage >=80){
+    return "B";
+  }else if(percentage >=70){
+    return "C";
+  }else if(percentage >=60){
+    return "D";
+  }else{
+    return "F";
+  }
+}
 
 /**
  * Checks if a `restaurant` object has a `reviews` property.
