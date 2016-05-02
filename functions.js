e719ab781b6fbe4dcef48b48144715a85e0edaf0
Lisa <misslisazhou@gmail.com>
2016-05-01 22:19:19 -1000

    create combine function

--- functions.js
+++ functions.js
@@ -230,7 +230,9 @@ function incrementReviews(restaurant) {
  * @param {string} word2
  * @return {string} joined the words joined with a space
  */
-
+function combine(word1,word2) {
+  return word1 + " " + word2;
+}
 
 /**
  * Returns a circle object with the properties `circumference` and `area`.
