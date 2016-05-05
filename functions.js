442e86f782fdece8f8e54f19355a52f7d45623f7
Nikki Y Kobayashi <nikz@MacBook-Pro-2.local>
2016-05-04 23:28:32 -1000

    createCircle

--- functions.js
+++ functions.js
@@ -254,3 +254,10 @@ function combine(word1,word2){
  * @return {object} circle
  */
 
+function createCircle(radius){
+	var circle = {
+		circumference: 2*Math.PI*radius,
+		area: Math.PI*radius*radius
+	};
+	return circle;
+}
