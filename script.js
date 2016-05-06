accd398169c5da1f7d2b1cc36a73160681d3a237
Tanachi <tanachige@gmail.com>
2016-05-05 14:31:12 -1000

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,278 @@
 // Please make sure all your code is added inside the `runAfterTests` function.
 function runAfterTests() {
-  /**
-   * Follow the comments below.
-   * Feel free to use `console.log` to see what values are being returned.
-   */
+  function numberToString(num){
 
-  // Create a variable called `myNumber`, and assign it to any number
+   return num.toString();
+}
 
-  /**
-   * Create another variable called `stringValue`.
-   * Assign it the value that's returned when you pass
-   * `myNumber into the function `numberToString`.
-   */
+function increase(num){
+  return num + 1;
+}
+/**
+ * Adds one to a given number.
+ * @param {number} n
+ * @return {number}
+ */
+function decrease(num){
+  return num-1;
+}
 
-  /**
-   * If you pass `myNumber` and `stringValue` into `areEqual`,
-   * what value will you get back? Why is that?
-   */
+/**
+ * Subtracts one from a given number.
+ * @param {number} n
+ * @return {number}
+ */
+
+function add(x,y){
+  return x+y;
+}
+
+/**
+ * Adds two numbers.
+ * @param {number} x
+ * @param {number} y
+ * @return {number} the sum
+ */
+function subtract(x,y){
+  return x-y;
+}
+
+
+/**
+ * Subtracts the second number from the first.
+ * @param {number} x
+ * @param {number} y
+ * @return {number} the difference
+ */
+ function multiply(x,y){
+  return x*y;
+}
+
+
+/**
+ * Multiplies two numbers.
+ * @param {number} x
+ * @param {number} y
+ * @return {number} the product
+ */
+function divide(x,y){
+  return x/y;
+}
+
+/**
+ * Divides the first number by the second.
+ * @param {number} x
+ * @param {number} y
+ * @return {number} the quotient
+ */
+function square(num){
+  return num*num;
+}
+
+/**
+ * Multiplies a number by itself.
+ * @param {number} x, number to be squared
+ * @return {number} squared
+ */
+
+function calculate(operation,x,y){
+
+if(operation == 'add'){
+  console.log (x + " + " + y + " = " + (x+y));
+  return (x+y);
+}
+else if(operation == 'subtract'){
+  console.log (x + " - " + y + " = " + (x-y));
+  return (x-y);
+}
+else if(operation == 'multiply'){
+  console.log (x + " * " + y + " = " + (x*y));
+  return (x*y);
+}
+else if(operation == 'divide'){
+  console.log(x + " / " + y + " = " + (x/y));
+  return (x/y);
+}
+}
+/**
+ * Performs a mathematical operation on two numbers.
+ * Also prints out the equation: (i.e.) "1 + 5 = 6" or "8 / 2 = 4".
+ * @param {string} operation "add", "subtract", "multiply", or "divide"
+ * @param {number} x
+ * @param {number} y
+ * @return {number} the result
+ */
+function isGreaterThan(a,b){
+  if(a > b)
+    return true;
+  else
+    return false;
+}
+
+/**
+ * Returns true if `a` is greater than `b`.
+ * @param {number} a
+ * @param {number} b
+ * @return {boolean} `a` is larger than `b`
+ */
+function isLessThan(a,b){
+  if(a < b)
+    return true;
+  else
+    return false;
+}
+
+
+/**
+ * Returns true if `a` is less than `b`.
+ * @param {number} a
+ * @param {number} b
+ * @return {boolean} `a` is smaller than `b`
+ */
+function areEqual(a,b){
+  if(a === b)
+    return true;
+  else
+    return false;
+}
+
+/**
+ * Returns true if `a` and `b` are equal.
+ * @param {number} a
+ * @param {number} b
+ * @return {boolean} the numbers are equal
+ */
+function minimum(a,b){
+  if(a > b)
+    return b;
+  else
+    return a;
+}
+
+
+/**
+ * Returns the smallest value of two numbers.
+ * @param {number} x
+ * @param {number} y
+ * @return {number} the smallest number
+ */
+
+
+/**
+ * Returns the largest value of two numbers.
+ * @param {number} x
+ * @param {number} y
+ * @return {number} the largest number
+ */
+
+function maximum(a,b){
+  if(a > b)
+    return a;
+  else
+    return b;
+}
+
+
+/**
+ * Returns true if `n` is even.
+ * @param {number} n
+ * @return {boolean} the number is even
+ */
+function isEven(n){
+  if(n % 2 === 0)
+    return true;
+  else
+    return false;
+}
+
+
+/**
+ * Returns true if `n` is odd.
+ * @param {number} n
+ * @return {boolean} the number is odd
+ */
+function isOdd(n){
+  if(n % 1 === 0)
+    return true;
+  else
+    return false;
+}
+
+/**
+ * Returns a letter grade.
+ * "A": 90-100%
+ * "B": 80-89%
+ * "C": 70-79%
+ * "D": 60-69%
+ * "F": 0-59%
+ * @param {number} score
+ * @param {number} total maximum possible score
+ * @return {string} the score represented as a letter grade
+ */
+
+function letterGrade(score, maxScore){
+  var percent = (score/maxScore);
+  if(percent >= (9/10))
+    return 'A';
+  else if(percent >= (8/10) && percent < (9/10))
+    return 'B';
+  else if(percent >= (7/10) && percent < (8/10))
+    return 'C';
+  else if(percent >= (6/10) && percent < (7/10))
+    return 'D';
+  else if(percent < (6/10))
+    return 'F';
+}
+
+/**
+ * Checks if a `restaurant` object has a `reviews` property.
+ * If it does, increase the property's `reviews` value by 1.
+ * If it does not, set the `reviews` value to 1.
+ * @param {object} restaurant   represents a restaurant object
+ * @return {object} restaurant
+ */
+function incrementReviews(restaurant){
+  var number = restaurant.reviews;
+  if(!number){
+    restaurant.reviews = 1;
+    return restaurant;
+  }
+  else{
+    restaurant.reviews = restaurant.reviews + 1;
+    return restaurant;
+  }
+}
+
+/**
+ * Joins two strings with a space.
+ * @param {string} word1
+ * @param {string} word2
+ * @return {string} joined the words joined with a space
+ */
+function combine(one,two){
+  var both = one + " " + two;
+  return both;
+}
+
+/**
+ * Returns a circle object with the properties `circumference` and `area`.
+ * Use Math.PI for the value π.
+ * (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/PI)
+ * @param {number} radius
+ * @return {object} circle
+ */
+
+function createCircle(radius){
+  var circle = {
+    circumference: 2*Math.PI * radius,
+    area: Math.PI * (radius * radius)
+  };
+  return circle;
+}
+var myNumber = 5;
+var stringValue = numberToString(myNumber);
+console.log(areEqual(myNumber,stringValue));
+// No they are not equal because of the different data types.
 
   /**
    * You may have noticed that the `calculate` function combined
@@ -28,22 +284,18 @@ function runAfterTests() {
   // Do you see any other functions where code could be reused?
 
   /**
-   * For your `letterGrade` function,
-   * what happens if you provide a `score` that is less than 0,
-   * or greater than the `total` value?
-   * Does it still work as expected? If it doesn't, how could you improve this?
-   */
-
-  /**
    * Your `combine` function looks okay,
    * but what if you wanted to join it with ANY string instead of a single space?
    * Create a new function called `improvedCombine`,
    * with the same parameters as `combine`, but also add a
    * third parameter called `glue` (a string value).
    */
+ function improvedCombine(one,two,glue){
+   return one + " " + glue + " " + two;
 
+ }
   // What happens if you were to uncomment the following lines?
 
-  // combine = improvedCombine;
-  // console.log(combine('combine', 'improved!', ' is '));
+ combine = improvedCombine;
+  console.log(combine('combine', 'improved!', ' is '));
 }
